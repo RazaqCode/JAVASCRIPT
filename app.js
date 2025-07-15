1aa7de4400541022b845be6420c0b804b524ed0a
RazaqCode <razaqrealm@gmail.com>
2025-07-15 22:53:35 +0530

    fizz

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-let x = 10, y = 20;
-[x, y] = [y, x];
-console.log("x:", x, "y:", y);
+for (let i = 1; i <= 20; i++) {
+  let output = "";
+  if (i % 3 === 0) output += "Fizz";
+  if (i % 5 === 0) output += "Buzz";
+  console.log(output || i);
+}
