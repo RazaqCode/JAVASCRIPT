e47b49c8833e93a1b2fdb03f4976a2f293cbd7ac
RazaqCode <razaqrealm@gmail.com>
2025-07-16 22:09:38 +0530

    forRev

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 let arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Output: [5, 4, 3, 2, 1]
+let reversedArr = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  reversedArr.push(arr[i]);
+}
+
+console.log(reversedArr); // Output: [5, 4, 3, 2, 1]
