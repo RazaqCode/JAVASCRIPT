cbdee41e3abe36eef77928f7554c8f42323ffced
RazaqCode <razaqrealm@gmail.com>
2025-07-16 22:04:33 +0530

    isBal

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-let nums = [10, 20, 30, 40];
-let total = nums.reduce((sum, val) => sum + val, 0);
-console.log("Total Sum:", total);
+function isBalanced(s) {
+  let stack = [];
+  let map = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+  };
+
+  for (let char of s) {
+    if (char === '(' || char === '{' || char === '[') {
+      stack.push(char);
+    } else if (char === ')' || char === '}' || char === ']') {
+      if (stack.pop() !== map[char]) return false;
+    }
+  }
+
+  return stack.length === 0;
+}
+
+// Example
+console.log(isBalanced("{[()]}")); // true
+console.log(isBalanced("{[(])}")); // false
