6a83004827067c177ab28fed636fefae88359e76
RazaqCode <razaqrealm@gmail.com>
2025-07-18 21:09:42 +0530

    slice

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-let a  = 1;
-console.log("iam ABDUL RAZAQ and iam  worlds number ", a + " richest man ");
+const mergeSortedArrays = (a, b) => {
+  const merged = [];
+  let i = 0, j = 0;
+
+  while (i < a.length && j < b.length) {
+    merged.push(a[i] < b[j] ? a[i++] : b[j++]);
+  }
+
+  return [...merged, ...a.slice(i), ...b.slice(j)];
+};
+
+// Test
+console.log(mergeSortedArrays([1, 4, 6], [2, 3, 5])); 
+// Output: [1, 2, 3, 4, 5, 6]
