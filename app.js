63b6d30a656182bfc8bc5b5c28d4bffa65910297
RazaqCode <razaqrealm@gmail.com>
2025-07-28 11:46:30 +0530

    correctNew

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-const isBalanced = (str) => {
-  const stack = [];
-  const pairs = { ')': '(', '}': '{', ']': '[' };
-
-  for (let char of str) {
-    if (['(', '{', '['].includes(char)) {
-      stack.push(char);
-    } else if (pairs[char]) {
-      if (stack.pop() !== pairs[char]) return false;
-    }
-  }
-
-  return stack.length === 0;
-};
-
-// Test
-console.log(isBalanced("({[]})"));  // true
-console.log(isBalanced("({[})"));   // false
+console.log("new corresction")
