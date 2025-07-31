16919f612da4ea86b1a4b9c59459e217c849106e
RazaqCode <razaqrealm@gmail.com>
2025-07-31 23:31:57 +0530

    opr

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("cool")
+function calculator(a, b, operator) {
+  switch(operator) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;
+    case '/': return b !== 0 ? a / b : 'Cannot divide by 0';
+    default: return 'Invalid operator';
+  }
+}
+
+console.log(calculator(10, 5, '+')); // 15
