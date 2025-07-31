0a8b9ae709940cfcdf5f2a22bc315ea4e603ed06
RazaqCode <razaqrealm@gmail.com>
2025-07-31 23:35:27 +0530

    setint

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-function isPalindrome(str) {
-  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-  return cleaned === cleaned.split('').reverse().join('');
+function countdown(seconds) {
+  let counter = seconds;
+  const interval = setInterval(() => {
+    console.log(counter);
+    counter--;
+    if (counter < 0) {
+      clearInterval(interval);
+      console.log("Time's up!");
+    }
+  }, 1000);
 }
 
-console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+countdown(5); // Counts down from 5 to 0
